b4d3f5b3f5963b5ff23cfa135e2b99daccbca865
Anxo Rodriguez <anxolin@gmail.com>
2021-11-17 09:51:36 +0000

    Add constant with versions

---
+++ constants.ts
@@ -0,0 +1,5 @@
+export const SITE_TITLE = 'CoW Protocol'
+export const URL_PRODUCTION = "https://cowswap.exchange"
+
+export const { version: WEB_VERSION } = require('./package.json')
+export const { version: CONTRACTS_VERSION } = require('@gnosis.pm/gp-v2-contracts/package.json')
